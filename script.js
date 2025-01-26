d4fd4cbfefa71b9570f06de3c51bdc4cfa232459
Daniel Schnieder <125045213+dschnieder@users.noreply.github.com>
2025-01-26 01:02:49 -0500

    Update script.js

--- script.js
+++ script.js
@@ -57,20 +57,20 @@ function myFunction() {
   }
 }
 
-var coll = document.getElementsByClassName("collapsible");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.maxHeight){
-      content.style.maxHeight = null;
-    } else {
-      content.style.maxHeight = content.scrollHeight + "px";
-    } 
+document.addEventListener("DOMContentLoaded", function () {
+    var coll = document.getElementsByClassName("collapsible");
+    for (var i = 0; i < coll.length; i++) {
+      coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight) {
+          content.style.maxHeight = null;
+        } else {
+          content.style.maxHeight = content.scrollHeight + "px";
+        }
+      });
+    }
   });
-}
 
 let slideIndex = 0;
 let slides, dots, timer;
